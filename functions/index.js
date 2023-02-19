4db57bc59fc02a56fc6bfd4a54403e24e625f99f
Billy Lo <billylo1@gmail.com>
2023-02-18 23:55:40 -0500

    added prefetch function so it's warmed up by the time submit is hit

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,7 @@
 import { ChatGPTAPI } from 'chatgpt'
 import whois from 'whois-json';
 import functions from "firebase-functions";
+import fetch from 'node-fetch';
 
 let apiClient;
 process.env.TZ = 'America/Toronto'
