c12adfe3d802df5e73943c1925089bc92d9ebd33
Billy Lo <billylo1@gmail.com>
2025-01-25 22:18:59 -0500

    fix for whois limit problem - use godaddy

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,5 @@
 import whois from 'whois-json';
-import functions from "firebase-functions";
+import functions from "firebase-functions/v1";
 
 process.env.TZ = 'America/Toronto'
 import OpenAI from "openai";
@@ -8,7 +8,7 @@ let chatHistory = [];
 
 async function sendMessage(user_input) {
 
-    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY, });
+    const openai = new OpenAI({ apiKey: process.env.OPENAI_API_KEY});
 
     const messageList = chatHistory.map(([input_text, completion_text]) => ({
         role: "user" === input_text ? "ChatGPT" : "user",
@@ -18,7 +18,7 @@ async function sendMessage(user_input) {
 
     try {
         const GPTOutput = await openai.chat.completions.create({
-            model: "gpt-4o-2024-05-13",
+            model: "chatgpt-4o-latest",
             messages: messageList,
         });
 
@@ -39,13 +39,24 @@ async function sendMessage(user_input) {
 
 }
 
+export const checkDomain = functions.https.onRequest(async (request, response) => {
+
+    try {
+        let domainName = request.query.domain;
+        const domainInfo = await checkWithGoDaddy(domainName);
+        return response.send(domainInfo);
+    } catch (e) {
+        console.error(e);
+        response.send(e);
+    }
+}
+)
+
 export const askdomaingpt = functions.runWith({
-    timeoutSeconds: 240, memory: "2GB"}).
-    https.onRequest(async (request, response) => {
+    timeoutSeconds: 240, memory: "2GB"}).https.onRequest(async (request, response) => {
 
     try {
         let prompt, initial;
-
         prompt = request.body.prompt;
         if (prompt == undefined) {
             return response.send('No prompt provided');
@@ -90,11 +101,32 @@ async function sendMessageAndCheckDomain(message) {
     }
 }
 
+function isValidDomainName(domainName) {
+    return /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(domainName);
+}
+
+async function checkWithGoDaddy(domainName) {
+    
+    domainName = encodeURIComponent(domainName);
+    console.log('Checking with GoDaddy...' + domainName);
+    const url = 'https://api.ote-godaddy.com/v1/domains/available?domain=' + domainName;
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': 'sso-key 3mM44WkB2Aifht_9ziNMQDFwKLrcFXUKoNWfg:9WKaxuKaCnN1oGbYHiU7UY'
+        }
+    });
+    let responseJson = await res.json();
+    console.log(responseJson);
+    return responseJson;
+}
 
 async function checkDomainAvailability(res) {
 
     let availableDomains = [];
     let done = false;
+    var checkCount = 0;
 
     do {
         // console.log(res.text);
@@ -104,19 +136,30 @@ async function checkDomainAvailability(res) {
         for (let domain of domains) {
             const splitText = domain.split(' ');
             if (splitText.length >= 2) {
-                const domainName = splitText[1].replace(/,/g, '');
+                var domainName = splitText[1].replace(/,/g, '');
+                domainName = domainName.replaceAll("**", "");
                 try {
-                    const domainInfo = await whois(domainName, { follow: 3, timeout: 3000 } );
-                    if (!domainInfo.hasOwnProperty('domainName')) {
-                        console.log(domainName + ' is available');
-
-                        availableDomains.push(domainName);
-                        if (availableDomains.length >= numOptions) {
-                            done = true;
-                            break;
-                        }
+                    // const domainInfo = await whois(domainName, { timeout: 7000 } );
+                    // if (domainInfo.hasOwnProperty('rateLimitExceededTryAgainAfter')) {
+                    //     availableDomains.push("Sorry, whois server quota has been exceeded. Please try again in a few minutes.");
+                    //     res.availableDomains = availableDomains;
+                    //     console.error('Rate limit exceeded');
+                    //     return Promise.resolve(res);                    
+                    // }
+                    if (isValidDomainName(domainName)) {
+                        const domainInfo = await checkWithGoDaddy(domainName);
+                        if (domainInfo.available) {
+                            console.log(domainName + ' is available');
+                            availableDomains.push(domainName);
+                            if (availableDomains.length >= numOptions) {
+                                done = true;
+                                break;
+                            } 
+                        } else {
+                            console.log(domainName + ' is not available');
+                        }                            
                     } else {
-                        console.log(domainName + ' is not available');
+                        console.warn('Invalid domain name: ' + domainName);
                     }
                 } catch (e) {
                     console.log('Error: ' + domainName);
@@ -128,13 +171,15 @@ async function checkDomainAvailability(res) {
             }
         }
 
-        if (!done) {
+        if (!done && checkCount < 5) {
             // console.log('Requesting more suggestions and checking availability...')
 
-            res = await sendMessage(`Ten more suggestions please`, {
+            res = await sendMessage(`Five more suggestions please`, {
                 conversationId: res.conversationId,
                 parentMessageId: res.id
             })
+
+            checkCount++;
         }
     } while (!done)
 
